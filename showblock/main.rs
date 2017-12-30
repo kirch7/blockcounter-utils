f42c09c04d3a74cc2782eef011d068b339138927
Cássio Kirch <cassiokirch@gmail.com>
2017-12-30 18:59:33 -0200

    showblock added.

---
+++ showblock/main.rs
@@ -0,0 +1,64 @@
+extern crate blockcounter;
+#[macro_use] extern crate clap;
+
+#[cfg(not(windows))] extern crate isatty;
+
+#[cfg(not(windows))]
+#[inline(always)]
+fn stdin_isatty() -> bool {
+    isatty::stdin_isatty()
+}
+
+#[cfg(windows)]
+#[inline(always)]
+fn stdin_isatty() -> bool {
+    true
+}
+
+fn main() {
+    let is_a_tty = stdin_isatty();
+    let yaml = match is_a_tty {
+        true  => {
+            load_yaml!("en_base.yml").clone()
+        },
+        false => {
+            load_yaml!("en.yml").clone()
+        },
+    };
+    let args = clap::App::from_yaml(&yaml)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .get_matches();
+
+    let tolerance = args
+        .value_of("TOLERANCE");
+    let tolerance = match tolerance {
+        Some(t) => t.parse().unwrap(),
+        None    => 2,
+    };
+
+    let block = args
+        .value_of("BLOCK")
+        .unwrap()
+        .parse()
+        .unwrap();
+    
+    if is_a_tty {
+        let file = args
+            .value_of("DATA")
+            .unwrap()
+            .to_string();
+        let file = std::fs::File::open(&file).unwrap();
+        let file = std::io::BufReader::new(file);
+        println!("{}", blockcounter::Blocks::new(tolerance, file).nth(block).unwrap());
+    } else {
+        use std::io::Read;
+        let stdin_ = std::io::stdin();
+        let mut s = String::new();
+        let _ = stdin_
+            .lock()
+            .read_to_string(&mut s)
+            .unwrap();
+        println!("{}", blockcounter::Blocks::new(tolerance, s.as_bytes()).nth(block).unwrap());
+    }
+}
